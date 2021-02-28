0eca9a5c0eb79444d9407300a759c3c00e35e023
Thibaut <thibaut.musslin@gmail.com>
2021-02-28 10:26:52 +0100

    Added Sample Settings

--- Hypixel PyImgui.py
+++ Hypixel PyImgui.py
@@ -1,13 +1,14 @@
 from dearpygui import core, simple
 
 
-def save_callback(sender, data):
-    print('Settings are still in developpement and not available.')
-
-
 core.set_main_window_size(500, 500)
 with simple.window("Hypixel Stats"):
-    core.add_checkbox("Settings", callback=save_callback)
     core.add_input_text("Player name", hint="Player name")
+    core.add_checkbox("Sample Settings")
+    core.add_checkbox("Sample Settings1")
+    core.add_checkbox("Sample Settings2")
+    core.add_checkbox("Sample Settings3")
+    core.add_checkbox("Sample Settings4")
+    core.add_checkbox("Sample Settings5")
 
 core.start_dearpygui(primary_window="Hypixel Stats")
