0b3be9a181552d648bc65f8bf25598bfb938cd7c
Thibaut <thibaut.musslin@gmail.com>
2021-02-28 10:01:25 +0100

    Hypixel PyImgui

---
+++ Hypixel PyImgui.py
@@ -0,0 +1,12 @@
+from dearpygui import core, simple
+
+
+def save_callback(sender, data):
+    print('Settings are still in developpement and not available.')
+
+
+with simple.window("Hypixel Stats"):
+    core.add_checkbox("Settings", callback=save_callback)
+    core.add_input_text("Player name", hint="Player name")
+
+core.start_dearpygui(primary_window="Hypixel Stats")
