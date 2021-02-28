e0032faa562ccab2c7243ba319a4e1758567aa76
Thibaut <thibaut.musslin@gmail.com>
2021-02-28 10:05:42 +0100

    Updated Main

--- Hypixel PyImgui.py
+++ Hypixel PyImgui.py
@@ -9,9 +9,4 @@ with simple.window("Hypixel Stats", width=500, height=500):
     core.add_checkbox("Settings", callback=save_callback)
     core.add_input_text("Player name", hint="Player name")
 
-
-with simple.window("Hypixel Stats"):
-    core.add_checkbox("Settings", callback=save_callback)
-    core.add_input_text("Player name", hint="Player name")
-
 core.start_dearpygui(primary_window="Hypixel Stats")
