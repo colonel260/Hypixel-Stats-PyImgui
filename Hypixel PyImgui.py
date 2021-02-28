d531eca67ed328254fb157cb111c7a84d32a9648
Thibaut <thibaut.musslin@gmail.com>
2021-02-28 10:04:43 +0100

    Update Main

--- Hypixel PyImgui.py
+++ Hypixel PyImgui.py
@@ -5,16 +5,13 @@ def save_callback(sender, data):
     print('Settings are still in developpement and not available.')
 
 
-<<<<<<< HEAD
 with simple.window("Hypixel Stats", width=500, height=500):
     core.add_checkbox("Settings", callback=save_callback)
     core.add_input_text("Player name", hint="Player name")
 
 
-=======
 with simple.window("Hypixel Stats"):
     core.add_checkbox("Settings", callback=save_callback)
     core.add_input_text("Player name", hint="Player name")
 
->>>>>>> main
 core.start_dearpygui(primary_window="Hypixel Stats")
