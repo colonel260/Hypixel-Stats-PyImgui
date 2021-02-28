0ae2e0fc1d06c8ee1e89a037ad6482053992f8e1
Thibaut <thibaut.musslin@gmail.com>
2021-02-28 13:09:09 +0100

    Added some features and rounded corners

--- Hypixel PyImgui.py
+++ Hypixel PyImgui.py
@@ -1,7 +1,16 @@
 from dearpygui import core, simple
 
 
+def Settings(sender, callback):
+    Stats = ("Imagine that this is " +
+             core.get_value("Player name") + "'s stats")
+    core.add_input_text("Stats", readonly=True, multiline=True,
+                        default_value=Stats, parent="Hypixel Stats")
+
+
 core.set_main_window_size(500, 500)
+core.set_main_window_title("Hypixel Stats")
+core.set_style_frame_rounding(4)
 with simple.window("Hypixel Stats"):
     core.add_input_text("Player name", hint="Player name")
     core.add_checkbox("Sample Settings")
@@ -10,5 +19,7 @@ with simple.window("Hypixel Stats"):
     core.add_checkbox("Sample Settings3")
     core.add_checkbox("Sample Settings4")
     core.add_checkbox("Sample Settings5")
+    core.add_spacing(count=20)
+    core.add_button("Lookup", callback=Settings, width=140, height=25)
 
 core.start_dearpygui(primary_window="Hypixel Stats")
