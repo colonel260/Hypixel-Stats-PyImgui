5f077f4516ea84b8edaa8a7ad69fb9012f1d4984
Thibaut <thibaut.musslin@gmail.com>
2021-05-11 18:09:49 +0200

    aestetic changes

--- Hypixel PyImgui.py
+++ Hypixel PyImgui.py
@@ -1,14 +1,13 @@
 from dearpygui import core, simple
 
 
-def Settings(sender, callback):
-    Stats = ("Imagine that this is " +
-             core.get_value("Player name") + "'s stats")
+def settings():
+    stats = ("Imagine that this is " + core.get_value("Player name") + "'s stats")
     core.add_input_text("Stats", readonly=True, multiline=True,
-                        default_value=Stats, parent="Hypixel Stats")
+                        default_value=stats, parent="Hypixel Stats")
 
 
-core.set_main_window_size(500, 500)
+core.set_main_window_size(500, 650)
 core.set_main_window_title("Hypixel Stats")
 core.set_style_frame_rounding(4)
 with simple.window("Hypixel Stats"):
@@ -19,7 +18,7 @@ with simple.window("Hypixel Stats"):
     core.add_checkbox("Sample Settings3")
     core.add_checkbox("Sample Settings4")
     core.add_checkbox("Sample Settings5")
-    core.add_spacing(count=20)
-    core.add_button("Lookup", callback=Settings, width=140, height=25)
+    core.add_spacing(count=50)
+    core.add_button("Lookup", callback=settings, width=140, height=25)
 
 core.start_dearpygui(primary_window="Hypixel Stats")
